8b337e56190e81f58ff1cbf712d57ad94342a8f8
José L. Patiño <jose.lpa@gmail.com>
2021-11-15 16:52:56 +0000

    Enforced responses from Client creation endpoint.

--- user_management/routers.py
+++ user_management/routers.py
@@ -1,13 +1,14 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 
 from user_management.core.dependencies import DBSession, get_database
-from user_management.schemas import NewClient
+from user_management.schemas import Client, NewClient
 from user_management.services.client import ClientService
 
 
 router = APIRouter()
 
 
-@router.post("/clients")
+@router.post("/clients", status_code=status.HTTP_201_CREATED, response_model=Client)
 def create_client(new_client: NewClient, db: DBSession = Depends(get_database)):
-    return ClientService(db).create_client(new_client)
+    client = ClientService(db).create_client(new_client)
+    return client
