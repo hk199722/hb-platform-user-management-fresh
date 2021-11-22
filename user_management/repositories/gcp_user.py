70951e1937c4f0365a0c5cd842d6c5cfe0dd7a68
José L. Patiño <jose.lpa@gmail.com>
2021-11-22 16:52:48 +0000

    AB-1340 Fixed API response in `GCPUserRepository.create` method.

--- user_management/repositories/gcp_user.py
+++ user_management/repositories/gcp_user.py
@@ -8,13 +8,19 @@ class GCPUserRepository(AlchemyRepository):
     schema = GCPUserSchema
 
     def create(self, schema: NewGCPUserSchema) -> GCPUserSchema:
+        """Overrides base `create` method to handle user roles creation for a given client, via the
+        `ClientUser` association object.
+        """
         response = super().create(schema=schema)
 
         if schema.role is not None:
+            # User role passed in. Create role for given Client.
             client_user = ClientUser(
                 client_uid=schema.role.client_uid, gcp_user_uid=response.uid, role=schema.role.role
             )
             self.db.add(client_user)
             self.db.commit()
+
+            response.clients = [client_user]
 
         return response
