bdc070b763eff444fedba25c17dfb333e48b910c
José L. Patiño <jose.lpa@gmail.com>
2021-11-24 17:09:15 +0000

    AB-1330 Fixed missing 'duplicate email' GCP exception, and added `GCPUser` data to exception context.

--- user_management/services/gcp_identity.py
+++ user_management/services/gcp_identity.py
@@ -1,6 +1,11 @@
 import logging
 
-from firebase_admin.auth import create_user, UidAlreadyExistsError, PhoneNumberAlreadyExistsError
+from firebase_admin.auth import (
+    create_user,
+    EmailAlreadyExistsError,
+    UidAlreadyExistsError,
+    PhoneNumberAlreadyExistsError,
+)
 
 from user_management.core.exceptions import ResourceConflictError
 from user_management.core.firebase import init_identity_provider_app
@@ -18,14 +23,41 @@ class GCPIdentityProviderService:
 
         try:
             create_user(uid=str(gcp_user.uid), display_name=gcp_user.name, email=gcp_user.email)
+        except EmailAlreadyExistsError as error:
+            logger.error(
+                "Unable to create user in GCP Identity Platform. Duplicated email: %s",
+                gcp_user.email,
+            )
+            raise ResourceConflictError(
+                context={
+                    "message": "Duplicated email.",
+                    "uid": str(gcp_user.uid),
+                    "email": gcp_user.email,
+                    "phone_number": gcp_user.phone_number,
+                }
+            ) from error
         except UidAlreadyExistsError as error:
             logger.error(
                 "Unable to create user in GCP Identity Platform. Duplicated UID: %s", gcp_user.uid
             )
-            raise ResourceConflictError(context={"uid": str(gcp_user.uid)}) from error
+            raise ResourceConflictError(
+                context={
+                    "message": "Duplicated UID.",
+                    "uid": str(gcp_user.uid),
+                    "email": gcp_user.email,
+                    "phone_number": gcp_user.phone_number,
+                }
+            ) from error
         except PhoneNumberAlreadyExistsError as error:
             logger.error(
                 "Unable to create user in GCP Identity Platform. Duplicated phone number: %s",
                 gcp_user.phone_number,
             )
-            raise ResourceConflictError(context={"phone_number": gcp_user.phone_number}) from error
+            raise ResourceConflictError(
+                context={
+                    "message": "Duplicated phone number.",
+                    "uid": str(gcp_user.uid),
+                    "email": gcp_user.email,
+                    "phone_number": gcp_user.phone_number,
+                }
+            ) from error
