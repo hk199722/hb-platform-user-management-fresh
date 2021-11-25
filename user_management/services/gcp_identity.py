7899fc347acd1d579a08be3f38831d11de735ad7
José L. Patiño <jose.lpa@gmail.com>
2021-11-25 09:28:21 +0000

    AB-1330 Added error handling for user not found in GCP-IP.

--- user_management/services/gcp_identity.py
+++ user_management/services/gcp_identity.py
@@ -3,8 +3,10 @@ import logging
 from firebase_admin.auth import (
     create_user,
     EmailAlreadyExistsError,
-    UidAlreadyExistsError,
     PhoneNumberAlreadyExistsError,
+    UidAlreadyExistsError,
+    update_user,
+    UserNotFoundError,
 )
 
 from user_management.core.exceptions import ResourceConflictError
@@ -16,13 +18,16 @@ logger = logging.getLogger(__name__)
 
 
 class GCPIdentityProviderService:
-    def sync_gcp_user(self, gcp_user: GCPUserSchema) -> None:
+    def sync_gcp_user(self, gcp_user: GCPUserSchema, update: bool = False) -> None:
         if not init_identity_provider_app():
             logger.debug("GCP Identity Provider not connected. New user not synced.")
             return
 
         try:
-            create_user(uid=str(gcp_user.uid), display_name=gcp_user.name, email=gcp_user.email)
+            if update is False:
+                create_user(uid=str(gcp_user.uid), display_name=gcp_user.name, email=gcp_user.email)
+            else:
+                update_user(uid=str(gcp_user.uid), display_name=gcp_user.name, email=gcp_user.email)
         except EmailAlreadyExistsError as error:
             logger.error(
                 "Unable to create user in GCP Identity Platform. Duplicated email: %s",
@@ -56,6 +61,19 @@ class GCPIdentityProviderService:
             raise ResourceConflictError(
                 context={
                     "message": "Duplicated phone number.",
+                    "uid": str(gcp_user.uid),
+                    "email": gcp_user.email,
+                    "phone_number": gcp_user.phone_number,
+                }
+            ) from error
+        except UserNotFoundError as error:
+            logger.error(
+                "Unable to modify user in GCP Identity Platform. User not found: %s",
+                gcp_user.uid,
+            )
+            raise ResourceConflictError(
+                context={
+                    "message": "User not found.",
                     "uid": str(gcp_user.uid),
                     "email": gcp_user.email,
                     "phone_number": gcp_user.phone_number,
