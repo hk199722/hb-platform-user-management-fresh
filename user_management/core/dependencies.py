e62106b1bcdf3ac666e035ff2f68fed0b821dda2
José L. Patiño <jose.lpa@gmail.com>
2021-12-20 09:51:37 +0000

    AB-1353 Implemented `get_user_info` core dependency.

--- user_management/core/dependencies.py
+++ user_management/core/dependencies.py
@@ -1,8 +1,13 @@
+import base64
+import binascii
+import json
 from typing import Generator, TypeVar
 
+from fastapi import Header
 from sqlalchemy.orm import scoped_session, Session
 
 from user_management.core.database import db_session_factory
+from user_management.core.exceptions import AuthenticationError
 
 
 DBSession = TypeVar("DBSession", scoped_session, Session)
@@ -16,3 +21,17 @@ def get_database() -> Generator[scoped_session, None, None]:
         yield db_session
     finally:
         db_session.remove()
+
+
+def get_user_info(x_apigateway_api_userinfo: str = Header(None)) -> dict:
+    """Looks up for a GCP Identity Platform header 'X-Apigateway-Api-Userinfo', where the request
+    user information is passed, encoded in Base64, after a successful JWT check which is performed
+    in GCP side.
+    """
+    if x_apigateway_api_userinfo is None:
+        raise AuthenticationError({"message": "Missing user information in request."})
+
+    try:
+        return json.loads(base64.b64decode(x_apigateway_api_userinfo))
+    except (binascii.Error, json.JSONDecodeError, UnicodeDecodeError) as error:
+        raise AuthenticationError({"message": "Invalid user information payload."}) from error
