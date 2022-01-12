454de0ce1e61f26ecfeee97c7523d3951777a034
José L. Patiño <jose.lpa@gmail.com>
2022-01-12 15:12:48 +0000

    (!) Testing service with no GSA account credentials defined in settings.

--- user_management/core/firebase.py
+++ user_management/core/firebase.py
@@ -1,10 +1,11 @@
-import json
+# import json
 import logging
 from functools import cache
 from typing import Optional
 
 from firebase_admin import App, initialize_app
-from firebase_admin.credentials import Certificate
+
+# from firebase_admin.credentials import Certificate
 
 from user_management.core.config.settings import get_settings
 
@@ -16,20 +17,18 @@ logger = logging.getLogger(__name__)
 def init_identity_platform_app() -> Optional[App]:
     """Cached function to initialize GCP Identity Platform / Firebase app."""
     settings = get_settings()
-    if not settings.gcp_credentials:
-        logger.warning("GCP Identity Platform NOT connected: GCP credentials not configured.")
-        return None
+    # if not settings.gcp_credentials:
+    #     logger.warning("GCP Identity Platform NOT connected: GCP credentials not configured.")
+    #     return None
+
+    # try:
+    #     gcp_credentials = Certificate(json.loads(settings.gcp_credentials.get_secret_value()))
+    # except Exception:  # pylint: disable=broad-except
+    #     logger.exception("GCP Identity Platform NOT connected: invalid GCP credentials.")
+    #     return None
 
     try:
-        gcp_credentials = Certificate(json.loads(settings.gcp_credentials.get_secret_value()))
-    except Exception:  # pylint: disable=broad-except
-        logger.exception("GCP Identity Platform NOT connected: invalid GCP credentials.")
-        return None
-
-    try:
-        app = initialize_app(
-            credential=gcp_credentials, options={"httpTimeout": settings.gcp_request_timeout}
-        )
+        app = initialize_app(options={"httpTimeout": settings.gcp_request_timeout})
     except Exception:  # pylint: disable=broad-except
         logger.exception("GCP Identity Platform NOT connected: unable to initialize app.")
         return None
