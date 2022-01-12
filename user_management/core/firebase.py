b5bd1597ee94f692c6ad5d11c515e7947f0499e1
José L. Patiño <jose.lpa@gmail.com>
2022-01-12 16:44:42 +0000

    AB-1397 Modified GCP-IP initialization to work without explicitly defining `GCP_CREDENTIALS` envirnoment variable.

--- user_management/core/firebase.py
+++ user_management/core/firebase.py
@@ -1,11 +1,10 @@
-# import json
+import json
 import logging
 from functools import cache
 from typing import Optional
 
 from firebase_admin import App, initialize_app
-
-# from firebase_admin.credentials import Certificate
+from firebase_admin.credentials import Certificate
 
 from user_management.core.config.settings import get_settings
 
@@ -15,20 +14,27 @@ logger = logging.getLogger(__name__)
 
 @cache
 def init_identity_platform_app() -> Optional[App]:
-    """Cached function to initialize GCP Identity Platform / Firebase app."""
-    settings = get_settings()
-    # if not settings.gcp_credentials:
-    #     logger.warning("GCP Identity Platform NOT connected: GCP credentials not configured.")
-    #     return None
+    """
+    Cached function to initialize GCP Identity Platform / Firebase app.
 
-    # try:
-    #     gcp_credentials = Certificate(json.loads(settings.gcp_credentials.get_secret_value()))
-    # except Exception:  # pylint: disable=broad-except
-    #     logger.exception("GCP Identity Platform NOT connected: invalid GCP credentials.")
-    #     return None
+    If an environment variable `GCP_CREDENTIALS` is set, the app will be initialized using those
+    credentials. Otherwise, it will use Google Application Default Credentials as per the Firebase
+    SDK implementation.
+    """
+    settings = get_settings()
+    gcp_credentials = None
+
+    if settings.gcp_credentials:
+        try:
+            gcp_credentials = Certificate(json.loads(settings.gcp_credentials.get_secret_value()))
+        except Exception:  # pylint: disable=broad-except
+            logger.exception("GCP Identity Platform NOT connected: invalid GCP credentials.")
+            return None
 
     try:
-        app = initialize_app(options={"httpTimeout": settings.gcp_request_timeout})
+        app = initialize_app(
+            credential=gcp_credentials, options={"httpTimeout": settings.gcp_request_timeout}
+        )
     except Exception:  # pylint: disable=broad-except
         logger.exception("GCP Identity Platform NOT connected: unable to initialize app.")
         return None
