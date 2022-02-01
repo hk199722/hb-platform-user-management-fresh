e56961afb4c48de4420640608de39f35400b56cb
José L. Patiño <jose.lpa@gmail.com>
2022-02-01 13:55:11 +0000

    AB-1379 Implemented `MailerService.welcome_message` method.

--- user_management/repositories/security_token.py
+++ user_management/repositories/security_token.py
@@ -1,3 +1,7 @@
+from pydantic import UUID4
+
+from sqlalchemy import select
+
 from user_management.models import SecurityToken
 from user_management.repositories.base import AlchemyRepository
 from user_management.schemas import SecurityTokenSchema
@@ -6,3 +10,10 @@ from user_management.schemas import SecurityTokenSchema
 class SecurityTokenRepository(AlchemyRepository):
     model = SecurityToken
     schema = SecurityTokenSchema
+
+    def get_user_token(self, gcp_user_uid: UUID4) -> SecurityTokenSchema:
+        token = (
+            self.db.execute(select(self.model).filter_by(gcp_user_uid=gcp_user_uid)).scalars().one()
+        )
+
+        return self._response(token)
--- user_management/services/mailer.py
+++ user_management/services/mailer.py
@@ -9,6 +9,7 @@ from pydantic import UUID4
 from user_management.core.config.settings import get_settings
 from user_management.core.dependencies import DBSession
 from user_management.repositories.gcp_user import GCPUserRepository
+from user_management.repositories.security_token import SecurityTokenRepository
 from user_management.services.gcp_identity import GCPIdentityPlatformService
 
 
@@ -22,6 +23,7 @@ class MailerService:
         settings = get_settings()
 
         self.gcp_user_repository = GCPUserRepository(db)
+        self.security_token_repository = SecurityTokenRepository(db)
         self.gcp_identity_service = GCPIdentityPlatformService()
         self.client = PublisherClient(
             publisher_options=PublisherOptions(
@@ -42,7 +44,47 @@ class MailerService:
         """Helper function to encode the message to be published in GCP Pub/Sub as needed."""
         return json.dumps(message).encode("utf-8")
 
+    def welcome_message(self, gcp_user_uid: UUID4) -> None:
+        """
+        Sends a message to a GCP Pub/Sub queue when a new user joins the platform. The message will
+        trigger an email to be sent to the new user, with a link to a platform screen that allows
+        the user to set up a login password.
+        """
+        gcp_user = self.gcp_user_repository.get(pk=gcp_user_uid)
+
+        try:
+            token = self.security_token_repository.get_user_token(gcp_user_uid=gcp_user_uid)
+        except AttributeError:
+            logger.error(
+                "Unable to send welcome email to %s (%s): no Security Token found.",
+                gcp_user.email,
+                gcp_user_uid,
+            )
+            return
+
+        message = {
+            "message_type": "WELCOME",
+            "email": gcp_user.email,
+            "context": {
+                "full_name": gcp_user.name,
+                "link": f"{get_settings().accounts_base_url}/new-user/set-password/{gcp_user.uid}/{token.uid}",
+            },
+        }
+        published = self.client.publish(self.topic_path, self.encode_message(message))
+        response = published.result()
+
+        logger.info(
+            "Welcome email with set password instructions sent to user %s (%s). Message ID: %s.",
+            gcp_user.email,
+            gcp_user_uid,
+            response,
+        )
+
     def reset_password_message(self, gcp_user_uid: UUID4) -> None:
+        """Sends a message to a GCP Pub/Sub queue when a registered user requests to reset the user
+        password. The message will trigger an email to be sent to the user with a link to a platform
+        screen to reset the password.
+        """
         gcp_user = self.gcp_user_repository.get(pk=gcp_user_uid)
         message = {
             "message_type": "PASSWORD_RESET",
