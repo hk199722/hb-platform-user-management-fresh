82e4017b0bca825be69c621faee08ee8d5e8e739
José L. Patiño <jose.lpa@gmail.com>
2022-04-07 10:05:38 +0100

    Testing Sentry integration (4)

--- user_management/routers/sentry.py
+++ user_management/routers/sentry.py
@@ -8,6 +8,12 @@ router = APIRouter()
 
 @router.get("/test")
 def test_sentry():
+    import requests
+
+    response = requests.get("https://sentry.io")
+    if response != 200:
+        raise ConnectionError("Sentry connection error.")
+
     raise Exception(
         f"Testing Sentry: {get_settings().sentry_dsn}; {get_settings().release}; {get_settings().google_project_id}."
     )
