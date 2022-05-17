154bee6a6855e7b702376fc414cdce4fdcf46a7e
Erustus Agutu <erustusagutu@gmail.com>
2022-05-17 11:34:27 +0100

    Fix docker image version
    * Remove DATABASE_URL from tracking

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,4 +4,5 @@ from dotenv import load_dotenv
 
 import user_management
 
+load_dotenv(f"{os.path.dirname(user_management.__path__[0])}/.env")
 load_dotenv(f"{os.path.dirname(user_management.__path__[0])}/tests/test.env", override=True)
